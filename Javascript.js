76dbc3d03d7e5b875bf59a93e0f377c4fe229a44
Ashok kumar <rajkumat18110@gmail.com>
2025-09-06 19:36:48 +0530

    message

---
+++ Javascript.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementById("hamburger");
+const left = document.getElementById("left");
+
+hamburger.addEventListener("click", () => {
+  left.classList.toggle("show");
+});
+
+
+console.log('hello');
